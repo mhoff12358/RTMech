96bf9dbc3a1af48c27a2a693167d8c28044d0f61
mhoff <mhoff12358@gmail.com>
2022-11-18 14:06:01 -0800

    Add turning that only does 45 degree increments.
    There's some real jankiness root-motion-ing the rotation because the
    start step callback happens mid-animation resolution. That should
    probably move to the update function somehow.

--- Player/WalkingState.gd
+++ Player/WalkingState.gd
@@ -5,8 +5,9 @@ class_name WalkingState
 @export var m_throttle_dead_zone: float = 0.1
 @export var m_throttle_adjust_speed: float = 0.5
 
-@export var m_desired_heading: float = 0
-@export var m_desired_heading_adjust_speed: float = 180
+@export var m_desired_heading_offset: float = 0
+@export var m_desired_heading_offset_adjust_speed: float = 180
+@export var m_heading_offset_max: float = 45
 
 @export_enum("Left", "Right") var m_placed_foot = 0
 
@@ -16,10 +17,12 @@ class_name WalkingState
 
 @onready var m_animation_tree: AnimationTree = m_context.get_registered_node("AnimationTree")
 var m_step_playback: AnimationNodeStateMachinePlayback
+var m_turn_waist_playback: AnimationNodeStateMachinePlayback
 
 @onready var m_footsteps: Footsteps = m_context.get_registered_node("Footsteps")
 
 @onready var m_waist: Node2D = m_context.get_registered_node_with_id("Node2D", "Waist")
+@onready var m_torso: Node2D = m_context.get_registered_node_with_id("Node2D", "Torso")
 @onready var m_right_foot: Node2D = m_context.get_registered_node_with_id("Node2D", "RightFoot")
 @onready var m_left_foot: Node2D = m_context.get_registered_node_with_id("Node2D", "LeftFoot")
 
@@ -28,16 +31,45 @@ var m_grounded_foot_position: Vector2 = Vector2.ZERO
 func start_step():
     m_animation_tree.set("parameters/AddWalk/add_amount", get_clipped_throttle())
 
+    apply_waist_rotation_to_body()
+    var heading_delta = min(m_heading_offset_max, max(-m_heading_offset_max, m_desired_heading_offset))
+    m_desired_heading_offset -= heading_delta
+
+    print("delta ", heading_delta)
+    if heading_delta > 0:
+        m_turn_waist_playback.travel("TurnWaistRight")
+    elif heading_delta < 0:
+        m_turn_waist_playback.travel("TurnWaistLeft")
+    print("amt", abs(heading_delta) / 45.0)
+#    m_animation_tree.set("parameters/AddTurnWaist/add_amount", abs(heading_delta) / 45.0)
+
+#    m_left_foot.rotation = heading_delta * PI / 180
+#    m_right_foot.rotation = heading_delta * PI / 180
+#    m_waist.rotation = heading_delta * PI / 180
+
+#    m_animation_tree.set("parameters/TurnWaistScale/scale", heading_delta / 360.0)
+#    m_animation_tree.set("parameters/TurnWaistSeek/seek", 0)
+#
+#    if heading_delta < 0:
+#        m_animation_tree.set("parameters/TurnWaistBlend/blend_amount", 0)
+#    else:
+#        m_animation_tree.set("parameters/TurnWaistBlend/blend_amount", 1)
+
 func place_right():
     m_placed_foot = 1
     m_grounded_foot_position = m_right_foot.global_position
+
+#    m_animation_tree.set("parameters/TurnWaistScale/scale", 0)
 
 func place_left():
     m_placed_foot = 0
     m_grounded_foot_position = m_left_foot.global_position
 
+#    m_animation_tree.set("parameters/TurnWaistScale/scale", 0)
+
 func _ready():
     m_step_playback = m_animation_tree.get("parameters/Step/playback")
+    m_turn_waist_playback = m_animation_tree.get("parameters/TurnWaist/playback")
 
 func _di_name():
     return "WalkingState"
@@ -49,15 +81,22 @@ func internal_process(delta: float):
         m_throttle += m_throttle_adjust_speed * delta
     m_throttle = max(-1, min(1, m_throttle))
 
-    print(m_throttle)
+#    if Input.is_action_pressed("WalkDirectionLeft"):
+#        m_desired_heading_offset -= m_desired_heading_offset_adjust_speed * delta
+#    if Input.is_action_pressed("WalkDirectionRight"):
+#        m_desired_heading_offset += m_desired_heading_offset_adjust_speed * delta
+    if Input.is_action_just_pressed("WalkDirectionLeft"):
+        m_desired_heading_offset -= 45.0
+    if Input.is_action_just_pressed("WalkDirectionRight"):
+        m_desired_heading_offset += 45.0
 
     var clipped_throttle = get_clipped_throttle()
     var current_walking_node = m_step_playback.get_current_node()
-    var currently_walking = (current_walking_node != "RESET")
+    var currently_walking = (current_walking_node != "Nothing")
     if (!currently_walking) and (clipped_throttle != 0):
         m_step_playback.travel("StepRight")
     elif currently_walking and (clipped_throttle == 0):
-        m_step_playback.travel("RESET")
+        m_step_playback.travel("Nothing")
 
     var current_grounded_foot_position: Vector2
     if m_placed_foot == 1:
@@ -72,8 +111,23 @@ func internal_process(delta: float):
     m_character_body.move_and_collide(body_offset)
 
 func apply_waist_rotation_to_body():
-    m_character_body.rotation = m_waist.rotation
-    m_waist.rotation = 0
+    var waist_position = m_waist.global_position
+    var left_foot_position = m_left_foot.global_position
+    var right_foot_position = m_right_foot.global_position
+
+    var waist_rotation = m_waist.global_rotation
+    m_character_body.rotation = waist_rotation
+    m_waist.rotation = waist_rotation
+
+#    var waist_rotation = m_waist.rotation
+#    m_waist.rotation = 0
+#    m_character_body.rotation += waist_rotation
+
+    #m_waist.global_position = waist_position
+    #m_left_foot.rotation = 0
+    #m_left_foot.global_position = left_foot_position
+    #m_right_foot.rotation = 0
+    #m_right_foot.global_position = right_foot_position
 
 func get_clipped_throttle():
     if abs(m_throttle) < m_throttle_dead_zone:
